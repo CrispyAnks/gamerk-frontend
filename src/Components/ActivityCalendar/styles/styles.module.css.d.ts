fab8a3ecfbc9ce6d4bddc571f0a9b488c2c33578
CrispyAnks <148548944+CrispyAnks@users.noreply.github.com>
2024-01-18 21:52:23 +0900

    Delete src directory

--- src/Components/ActivityCalendar/styles/styles.module.css.d.ts
+++
@@ -1,6 +0,0 @@
-export const calendar: string;
-export const container: string;
-export const footer: string;
-export const legendColors: string;
-export const loadingAnimation: string;
-export const scrollContainer: string;
