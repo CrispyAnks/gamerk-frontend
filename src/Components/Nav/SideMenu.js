a1ce9bd1ad914e5b000379a1f6b938b793c5f8e7
CrispyAnks <148548944+CrispyAnks@users.noreply.github.com>
2024-01-30 10:17:43 +0900

    Update SideMenu.js

--- src/Components/Nav/SideMenu.js
+++ src/Components/Nav/SideMenu.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function SideMenu(props) {
-  const [isShow, setIsShow] = useState(false);
-  const handleClose1 = () => setIsShow(false);
-  const handleShow1 = () => setIsShow(true);
+  const {isShow, handleClose1 } = props;
 
   return (
     <>
-      <Button variant="dark" onClick={handleShow1}>
-        Gamerk
-      </Button>
-
-      <Offcanvas show={isShow} onHide={handleClose1}>
+      <Offcanvas id={`offcanvasNavbar-expand`} show={isShow} onHide={handleClose1}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>M E N U</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>
-          <ListGroup defaultActiveKey="#link1">
-          <ListGroup.Item action>
-              <NavLink to='/' className='nav-link'>H O M E</NavLink>
+        <Offcanvas.Body style={{backgroundcolor:'rgba(246, 173, 73, 0.95)', textAlign:'center'}}>
+          <ListGroup defaultActiveKey="#link1" >
+            <ListGroup.Item action >
+              <NavLink to='/' className='nav-link' style={{fontWeight: 'bold' }}>ホーム</NavLink>
             </ListGroup.Item>
             {props.userid == null &&<ListGroup.Item action>
-             <NavLink to='/login' className='nav-link'>L O G I N</NavLink>
+             <NavLink to='/login' className='nav-link' style={{fontWeight: 'bold' }}>ログイン</NavLink>
             </ListGroup.Item>}
             {props.userid == null &&<ListGroup.Item action>
-             <NavLink to='/signup' className='nav-link'>S I G N  U P</NavLink>
+             <NavLink to='/signup' className='nav-link' style={{fontWeight: 'bold' }}>サインアップ</NavLink>
             </ListGroup.Item>}
             {props.userid != null &&<ListGroup.Item action>
-              <NavLink to={'/mypage/' + props.userid} className='nav-link'>M Y P A G E</NavLink>
+              <NavLink to={'/mypage/' + props.userid} className='nav-link' style={{fontWeight: 'bold' }}>マイページ</NavLink>
             </ListGroup.Item>}
             {props.userid != null &&
             <ListGroup.Item action>
-             <NavLink to='/logout' className='nav-link'>L O G O U T</NavLink>
+             <NavLink to='/logout' className='nav-link' style={{fontWeight: 'bold' }}>ログアウト</NavLink>
             </ListGroup.Item>}
             <ListGroup.Item action>
-              <NavLink to='/about' className='nav-link'>A B O U T  M E</NavLink>
+              <NavLink to='https://sei-portfolio-e34a1928f16c.herokuapp.com/' className='nav-link'>A B O U T  M E</NavLink>
             </ListGroup.Item>
           </ListGroup>
         </Offcanvas.Body>
