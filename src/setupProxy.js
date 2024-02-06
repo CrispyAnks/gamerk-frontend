76f816553ae2482e0fe09471c6df791f15c7ba5a
CrispyAnks <148548944+CrispyAnks@users.noreply.github.com>
2024-02-06 11:27:58 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware: proxy} = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         proxy('/api',{
-            target:'https://gamerk-d9b989fca00f.herokuapp.com/',
+            target:'https://hal-th23-gamerk-ih15-a26.azurewebsites.net/',
             changeOrigin: true,
             pathRewrite:{'^/api':''}
         })
